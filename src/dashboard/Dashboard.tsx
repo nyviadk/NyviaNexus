5b92f4949c0a67a66aff4be1d065e311d41a5eb7
nyviadk <nyvia.dk@gmail.com>
2025-12-23 17:41:51 +0100

    grid-cols-5

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1240,7 +1240,7 @@ export const Dashboard = () => {
               </div>
             </header>
             <div className="flex-1 overflow-y-auto p-8">
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4">
                 {(isViewingInbox
                   ? inboxData?.tabs || []
                   : windows.find((w) => w.id === selectedWindowId)?.tabs || []
